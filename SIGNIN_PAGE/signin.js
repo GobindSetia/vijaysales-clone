649a1d5c432a07caf4a7eec38fc00011b2fb6212
riteshjk <96072567+riteshjk@users.noreply.github.com>
2022-01-20 21:44:36 +0530

    more changes

---
+++ SIGNIN_PAGE/signin.js
@@ -0,0 +1,81 @@
+let conta = document.getElementById("conta")
+
+let b=`<h2>LOGIN</h2>
+<p>Get access to your account</p>
+<input type="text" id="email1" placeholder="Email Or Mobile Number"><br>
+
+<input type="text" id="pass1" placeholder="Enter your password here"><br>
+
+<button onclick="getdata(event)">CONTINUE</button>
+<div id="main1">
+<div id="inst1">
+    <p>OR Login Using</p>
+</div>
+<div id="image">
+    <img src="https://d2xamzlzrdbdbn.cloudfront.net/imagesrewamp/checkout/login-via-facebook.png" class="one">
+    <img src="https://d2xamzlzrdbdbn.cloudfront.net/imagesrewamp/checkout/login-via-google.png" class="two one">
+</div>
+</div>
+<div id="newone1">
+    <p>New to Vijay Sales?</p>
+    <p class="color"> <a href="../SIGNUP_PAGE/signup.html">Create Account</a></p>
+</div>
+ 
+`
+conta.innerHTML=b
+
+let email = document.getElementById("email1").value
+
+function getdata(event){
+    event.preventDefault();
+let email = document.getElementById("email1").value
+}
+
+// document.querySelector("#info").addEventListener("submit",data);
+let userData=JSON.parse(localStorage.getItem("userDatabase"));
+let userData1=JSON.parse(localStorage.getItem("userDatabase1"));
+
+// console.log(arr)
+var flag = false;
+function getdata(event){
+   event.preventDefault();
+   
+   let lemail=document.getElementById("email1").value;
+   let lpass=document.getElementById("pass1").value
+   
+userData.map(function(ele){
+if(lemail==ele.email  ){
+    flag=true;
+    // check()
+    
+}
+
+})
+console.log(flag)
+// check();
+var a=false;
+
+userData1.map(function(ele){
+    if(lpass==ele.pass  ){
+        a=true;
+        // check()
+        
+    }
+   
+    })
+    console.log(a)
+    // check();
+    
+check(flag,a);
+}
+function check(flag,a){
+if(flag == true && a==true) {
+    window.location.href="home.html"
+}
+else{
+    alert("wrong credentials")
+}
+}
+
+
+
---
+++ components/footer.js
@@ -0,0 +1,119 @@
+function footer(){
+    return `<div id="Footer">
+    <div>
+    <h4>USEFUL LINKS</h4>
+    <p>HOME</p>
+    <p>ABOUT US</p>
+    <p>STORE LOCATOR</p>
+    <p>CAREERS</p>
+    <p>CONTACT US</p>
+    <p>BRAND STORES</p>
+    <p>E-WASTE</p>
+    <p>BLOG</p>
+    <p>LOYALTY</p>
+    <p>GIFTVOUCHERS</p>
+    </div>
+    <div>
+        <h4>CATEGORIES</h4>
+        <p>LAPTOP & PRINTER</p>
+        <p>MOBILES & TABLETS</p>
+        <p>WASHING MACHINE</p>
+        <p>TV & ENTERTAINMENT</p>
+        <p>HOME APPLIANCES</p>
+        <p>KITCHEN APPLIANCES</p>
+        <p>CAMERA</p>
+        <p>PERSONAL CARE</p>
+        <p>ACCESSORIES</p>
+        
+    </div>
+    <div>
+       <h4> MY ACCOUNT</h4>  
+       <p>MY ACCOUNT</p> 
+       <p>MY ORDERS</p> 
+    </div>
+    <div>
+       <h4>HELP DESK</h4> 
+       <p>TERMS OF USE</p>  
+       <p>PRIVACY POLICY</p> 
+       <p>PAYMENT & RETURNS</p>
+       <p>SHIPPING OPTION</p>
+       <p>HELP/FAQ</p>
+       <p>LOYALTY PROGRAMME</p>
+    </div>
+    <div>
+       <h4>NEWSLETTER</h4> 
+        <p>Stay in touch with us, get product updates, offers, discounts directly to your inbox</p>
+        <input type="text" id="bar" placeholder="Please Provide your Email id here">
+        <p>We'll never share your email address with a third party.</p>
+        <h4>FOLLOW US ON</h4>
+        <div id="k">
+            <img src="https://d2xamzlzrdbdbn.cloudfront.net/imgwebsite/follow-us-on-facebook.png" >
+            <img src="https://d2xamzlzrdbdbn.cloudfront.net/imgwebsite/follow-us-on-twitter.png" >
+            <img src="https://d2xamzlzrdbdbn.cloudfront.net/imgwebsite/follow-us-on-youtube.png" >
+            <img src="https://d2xamzlzrdbdbn.cloudfront.net/imgwebsite/follow-us-on-instagram.png" >
+            <img src="https://d2xamzlzrdbdbn.cloudfront.net/imgwebsite/follow-us-on-whatsapp.png" >
+        </div>
+        <p>FOR ONLINE ORDER</p>
+        <p>+91 7303434043</p>
+        <p>[ Mon To Sun : 10:00 AM To 6:00 PM ]</p>
+        
+
+    </div>
+</div>
+<div id="foot">
+<div id="header">
+    <h1>vijay Sales</h1>
+</div>
+<div id="pop">
+    <p class="sty">&nbsp;AGRA | &nbsp;AHMEDABAD | &nbsp;AURANGABAD | &nbsp;PUNE | &nbsp;BANGALORE | &nbsp;BHAVNAGAR | &nbsp;CHENNAI | &nbsp;COIMBATORE | &nbsp;DELHI | &nbsp;FARIDABAD | &nbsp;GURGAON | &nbsp;HYDERABAD | &nbsp;INDIRAPURAM | &nbsp;INDORE | &nbsp;JAIPUR | &nbsp;KAKINADA | &nbsp;KOLHAPUR | &nbsp;KOLKATA | &nbsp;LUCKNOW | &nbsp;MEERUT | &nbsp;MUMBAI</p>
+    <p class="sty1 sty">&nbsp;MYSORE | &nbsp;NAGPUR | &nbsp;NASHIK | &nbsp;NAVI MUMBAI | &nbsp;NOIDA | &nbsp;PALGHAR | &nbsp;PANIPAT | &nbsp;PATNA | &nbsp;RAJAHMUNDRY | &nbsp;RAJKOT | &nbsp;SURAT | &nbsp;THANE | &nbsp;TIRUPATI | &nbsp;VADODARA | &nbsp;VIJAYAWADA | &nbsp;VISAKHAPATNAM | &nbsp;WARANGAL</p>
+</div>
+</div>
+<div>
+    <div id="sty">
+
+    <img src="https://d2xamzlzrdbdbn.cloudfront.net/imagesrewamp/visa.png" id="a">
+    <img src="https://d2xamzlzrdbdbn.cloudfront.net/imagesrewamp/master-card.png" id="b">
+    <img src="https://d2xamzlzrdbdbn.cloudfront.net/imagesrewamp/amex-american-express.png" id="c">
+    <img src="https://d2xamzlzrdbdbn.cloudfront.net/imagesrewamp/pine-labs.png" id="d">
+    <img src="https://d2xamzlzrdbdbn.cloudfront.net/imagesrewamp/razorpay_tiny.png" id="f">
+</div>
+
+</div>
+<div id="z">
+<div>
+<p class="st"><span id="l"> LAPTOP & PRINTER</span>
+         &nbsp; Laptops | &nbsp;Desktops | &nbsp;Printers | &nbsp;MonitorEntry | &nbsp;LaptopsMainstream | &nbsp;LaptopsPremium | &nbsp;LaptopsGaming | &nbsp;LaptopsConvertibleLaptops | &nbsp;2 in 1 Laptops | &nbsp;Graphic Performance Laptops | &nbsp;Monochrome | &nbsp;PrinterColor |  &nbsp;PrinterInkjet PrinterLaser Printer | &nbsp;Multi Function Printer | &nbsp;Single Function Printer | &nbsp;Full HD MonitorGaming |  &nbsp;MonitorHD Monitor | &nbsp;All in One DesktopsCPU | &nbsp;iMacMac MiniHard |  &nbsp;DisksBackpack & Cases | &nbsp;PendrivesMouseRouter | &nbsp;SoftwareKeyboards</p>
+         </div>
+      
+<div>
+
+<p class="st1"><span id="l"> MOBILES & TABLETS</span> &nbsp;iPhones | &nbsp;Smartphones | &nbsp;Basic Phones | &nbsp;Tablets | &nbsp;iPad | &nbsp;Power Banks | &nbsp;Chargers | &nbsp;CablesCases & CoversMemory | &nbsp;CardCar | &nbsp;ChargerGimbal |&nbsp;Smart Watches & Bands | &nbsp;Smart Watch | &nbsp;StrapsSmart Watch | &nbsp;ChargerAirPods | &nbsp;AirPods ProAirPods | &nbsp;MaxApple  |&nbsp;WatchAirTagsCases & Covers | &nbsp;Adapters & CablesTWS (True Wireless Stereo Buds) | &nbsp;NeckbandHeadphones |&nbsp;EarphonesPortable Speaker</p>
+
+</div>
+<div>
+<p class="st2"> <span id="l">WASHING MACHINE</span> &nbsp;Fully Automatic Top Load | &nbsp;Semi Automatic Top Load | &nbsp;Fully Automatic Front Load | &nbsp;Washer with Dryer</p>
+</div>
+
+<div>
+<p class="st3"> <span id="l">TV & ENTERTAINMENT</span> &nbsp;Televisions | &nbsp;SoundbarParty | &nbsp;SpeakersMusic | &nbsp;PlayerSpeaker | &nbsp;Streaming Device | &nbsp;Set Top Box | &nbsp;Home Theater | &nbsp;Smart Speaker | &nbsp;PlayStation | &nbsp;XboxController | &nbsp;Play Station | &nbsp;GamesXbox Games</p>
+
+</div>
+<div >
+<p class="st6"> <span id="l">HOME APPLIANCES</span> &nbsp;Air Conditioners | &nbsp;Air Cooler | &nbsp;Air Purifier | &nbsp;Refrigerators | &nbsp;Dishwasher | &nbsp;DehumidifierGeysers & Heaters | &nbsp;Sanitizing Equipments | &nbsp;Steam Iron | &nbsp;Vacuum Cleaner | &nbsp;Water Dispenser | &nbsp;DustbinGarment Steamer | &nbsp;Spray Dispenser | &nbsp;Stabilizer | &nbsp;Tower FanUV CaseRoom | &nbsp;HeaterIron | &nbsp;Smart Home Kit | &nbsp;Deep FreezerIndoor | &nbsp;LightingVISI COOLER
+</p>
+</div>
+<div >
+    <p id="st7"><span id="l">KITCHEN APPLIANCES</span>&nbsp;Atta Maker | &nbsp;Built In Ovens | &nbsp;ChimneyChopper | &nbsp;Coffee Maker | &nbsp;CooktopFood Processor | &nbsp;Air Fryer | &nbsp;Hand Blender | &nbsp;HOBInduction Cooker | &nbsp;Juicer | &nbsp;Juicer Mixer Grinder | &nbsp;KettleMicrowave | &nbsp;MixerOven | &nbsp;Toaster Grillers | &nbsp;Popup Toaster | &nbsp;Rice Cooker | &nbsp;Sandwich Toaster | &nbsp;Vegetable Purifier | &nbsp;Water Purifier | &nbsp;Wet Grinder | &nbsp;Kadhai & WoksSandwich Griller | &nbsp;TawaFry PanBarbecues | &nbsp;Cookware | &nbsp;SetCookware | &nbsp;Stand Mixer2 Jar3 Jar4  | &nbsp;JarBullet Blender | &nbsp;Power BlenderStand  | &nbsp;MixerHand Blender | &nbsp;ChimneyHOBBuilt In Ovens
+    </p>
+</div>
+<div class="st4"> 
+<p ></p>
+</div>
+<div id="st5">
+<p>© 2007-2021 VijaySales.com</p>
+<p>Web Design by Aurus</p>
+</div>
+</div>`
+}
+export default footer
